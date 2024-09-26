32124d7a83e4b4348e17a7859758baccc1eb71cc
Youssif Mohamed elsawi <100567818+youssifelsawi@users.noreply.github.com>
2024-09-26 20:05:04 +0300

    Add files via upload

---
+++ src/Components/pages/BlogDetails/BlogDetails.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../../shared/Header";
+import Footer from "../../shared/Footer";
+import mainContent from './mainContent';
+import './BlogDetails.css';
+
+function BlogDetails() {
+  return (
+    <>
+      <Header />
+      <mainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default BlogDetails;
